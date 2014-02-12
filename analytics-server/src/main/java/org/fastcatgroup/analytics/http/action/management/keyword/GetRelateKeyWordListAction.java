29b7556a4e32b83f31f16eebb418585556cd23ad
swsong-dev@websqrd.com <swsong-dev@websqrd.com>
2014-02-12 18:18:58 +0900

    리소스 close처리.

--- analytics-server/src/main/java/org/fastcatgroup/analytics/http/action/management/keyword/GetRelateKeyWordListAction.java
+++ analytics-server/src/main/java/org/fastcatgroup/analytics/http/action/management/keyword/GetRelateKeyWordListAction.java
@@ -3,6 +3,8 @@ package org.fastcatgroup.analytics.http.action.management.keyword;
 import java.io.Writer;
 import java.util.List;
 
+import org.fastcatgroup.analytics.db.AnalyticsDBService;
+import org.fastcatgroup.analytics.db.MapperSession;
 import org.fastcatgroup.analytics.db.mapper.RelateKeywordMapper;
 import org.fastcatgroup.analytics.db.vo.RelateKeywordVO;
 import org.fastcatgroup.analytics.http.ActionAuthority;
@@ -10,11 +12,10 @@ import org.fastcatgroup.analytics.http.ActionMapping;
 import org.fastcatgroup.analytics.http.action.ActionRequest;
 import org.fastcatgroup.analytics.http.action.ActionResponse;
 import org.fastcatgroup.analytics.http.action.AuthAction;
-import org.fastcatgroup.analytics.keyword.KeywordService;
 import org.fastcatgroup.analytics.service.ServiceManager;
 import org.fastcatgroup.analytics.util.ResponseWriter;
 
-@ActionMapping(value="/management/keyword/relate/list", authority=ActionAuthority.Keyword)
+@ActionMapping(value = "/management/keyword/relate/list", authority = ActionAuthority.Keyword)
 public class GetRelateKeyWordListAction extends AuthAction {
 
 	@Override
@@ -23,49 +24,55 @@ public class GetRelateKeyWordListAction extends AuthAction {
 		String search = request.getParameter("search");
 		int start = request.getIntParameter("start");
 		int length = request.getIntParameter("length");
-		
-		KeywordService keywordService = ServiceManager.getInstance().getService(KeywordService.class);
-		
-		RelateKeywordMapper relateKeywordMapper = keywordService.getMapperSession(RelateKeywordMapper.class).getMapper();
-		
-		String whereCondition = "";
-		
-		if(search!=null && !"".equals(search)) {
-			whereCondition = " and keyword = '"+search+"' ";
-		}
-		
-		Writer writer = response.getWriter();
-		ResponseWriter resultWriter = getDefaultResponseWriter(writer);
-		
-		int totalSize = 0;
-		int filteredSize = 0;
-		
-		resultWriter.object().key("list").array();
-		
-		if(relateKeywordMapper!=null) {
-			totalSize = relateKeywordMapper.getCount(categoryId);
-			filteredSize = relateKeywordMapper.getCountByWhereCondition(categoryId, whereCondition);
-			
-			if(length==-1) {
-				length = totalSize;
+
+		AnalyticsDBService keywordService = ServiceManager.getInstance().getService(AnalyticsDBService.class);
+
+		MapperSession<RelateKeywordMapper> mapperSession = keywordService.getMapperSession(RelateKeywordMapper.class);
+
+		try {
+			RelateKeywordMapper relateKeywordMapper = mapperSession.getMapper();
+
+			String whereCondition = "";
+
+			if (search != null && !"".equals(search)) {
+				whereCondition = " and keyword = '" + search + "' ";
 			}
-			
-			if(totalSize > 0) {
-			
-				List<RelateKeywordVO> list = relateKeywordMapper.getEntryListByWhereCondition(categoryId, whereCondition, start, length);
-				for(RelateKeywordVO vo : list) {
-					
-					resultWriter.object().key("ID").value(vo.getId())
-						.key("KEYWORD").value(vo.getKeyword())
-						.key("VALUE").value(vo.getValues()).endObject();
+
+			Writer writer = response.getWriter();
+			ResponseWriter resultWriter = getDefaultResponseWriter(writer);
+
+			int totalSize = 0;
+			int filteredSize = 0;
+
+			resultWriter.object().key("list").array();
+
+			if (relateKeywordMapper != null) {
+				totalSize = relateKeywordMapper.getCount(categoryId);
+				filteredSize = relateKeywordMapper.getCountByWhereCondition(categoryId, whereCondition);
+
+				if (length == -1) {
+					length = totalSize;
+				}
+
+				if (totalSize > 0) {
+
+					List<RelateKeywordVO> list = relateKeywordMapper.getEntryListByWhereCondition(categoryId, whereCondition, start, length);
+					for (RelateKeywordVO vo : list) {
+
+						resultWriter.object().key("ID").value(vo.getId()).key("KEYWORD").value(vo.getKeyword()).key("VALUE").value(vo.getValue()).endObject();
+					}
 				}
 			}
+
+			resultWriter.endArray();
+
+			resultWriter.key("totalSize").value(totalSize).key("filteredSize").value(filteredSize);
+			resultWriter.endObject();
+			resultWriter.done();
+		} finally {
+			if (mapperSession != null) {
+				mapperSession.closeSession();
+			}
 		}
-		
-		resultWriter.endArray();
-		
-		resultWriter.key("totalSize").value(totalSize).key("filteredSize").value(filteredSize);
-		resultWriter.endObject();
-		resultWriter.done();
 	}
 }

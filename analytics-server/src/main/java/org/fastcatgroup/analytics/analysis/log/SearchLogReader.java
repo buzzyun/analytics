d0c0e3c3f277f27f11d385d338ee87398dc89cec
Yongmo Jeong <lupfeliz@gmail.com>
2014-03-20 19:47:48 +0900

    FileLogger -> FileListLogger 로 변경

--- analytics-server/src/main/java/org/fastcatgroup/analytics/analysis/log/SearchLogReader.java
+++ analytics-server/src/main/java/org/fastcatgroup/analytics/analysis/log/SearchLogReader.java
@@ -3,9 +3,9 @@ package org.fastcatgroup.analytics.analysis.log;
 import java.io.File;
 import java.io.IOException;
 
-public class SearchLogReader extends FileLogReader<SearchLog> {
+public class SearchLogReader extends FileListLogReader<SearchLog> {
 
-	public SearchLogReader(File file, String encoding) throws IOException {
+	public SearchLogReader(File[] file, String encoding) throws IOException {
 		super(file, encoding);
 	}
 

b742cee6404528f08bbe501958c25eb89f5d9d8b
swsong-dev@websqrd.com <swsong-dev@websqrd.com>
2014-04-29 15:11:52 +0900

    ..

--- analytics-server/src/test/java/org/fastcatsearch/analytics/analysis/RollingRawLoggerTest.java
+++ analytics-server/src/test/java/org/fastcatsearch/analytics/analysis/RollingRawLoggerTest.java
@@ -11,7 +11,7 @@ public class RollingRawLoggerTest {
 	@Test
 	public void test() {
 		File baseDir = new File("/tmp");
-		RollingRawLogger logger = new RollingRawLogger(baseDir, "total", "tmp.log");
+		RollingRawLogger logger = new RollingRawLogger(baseDir, "tmp.log");
 		
 		Random r = new Random(System.currentTimeMillis());
 		for (int i = 0; i < 100000; i++) {
